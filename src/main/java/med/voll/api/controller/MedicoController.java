541ed4eef30adf4cf386717191f9eda2a8b54b50
Milene <milenefaria33@gmail.com>
2024-10-25 13:00:06 -0300

    feat: criacao da listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,18 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +31,11 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico cadastroMedico) {
         var medico = new Medico(cadastroMedico);
         repository.save(medico);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedModel> listar(@PageableDefault(size = 2) Pageable pageable, PagedResourcesAssembler assembler) {
+        Page<DadosListagemMedico> page = repository.findAll(pageable).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(assembler.toModel(page));
     }
 }
