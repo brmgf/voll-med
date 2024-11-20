615349cd288a7426bfdf0957e1572ec32766faaf
Milene <milenefaria33@gmail.com>
2024-11-20 15:30:52 -0300

    feat: adicionando titulo e descricao api swagger

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,11 @@ public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info().title("Voll.med API")
+                        .description("API Rest para agendamento e cancelamento de consultas."))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
+                );
     }
 }
